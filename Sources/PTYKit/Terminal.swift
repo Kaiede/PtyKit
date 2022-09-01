ab5d62d7911eca78e103f239414c7ea4ed8db7fa
Kaiede <user@biticus.net>
2022-09-01 07:42:20 -0700

    Set Window Size on both Handles

--- Sources/PTYKit/Terminal.swift
+++ Sources/PTYKit/Terminal.swift
@@ -170,11 +170,23 @@ public final class PseudoTerminal {
 extension PseudoTerminal {
     public func setWindowSize(columns: UInt16, rows: UInt16) throws {
         var size = winsize(ws_row: rows, ws_col: columns, ws_xpixel: 0, ws_ypixel: 0)
-        let result = ioctl(childHandle.fileDescriptor, TIOCSWINSZ, &size)
 
-        if let errorCode = POSIXErrorCode(rawValue: result) {
-            throw POSIXError(errorCode)
+        // Parent Terminal
+        do {
+            let result = ioctl(hostHandle.fileDescriptor, TIOCSWINSZ, &size)
+            if let errorCode = POSIXErrorCode(rawValue: result) {
+                throw POSIXError(errorCode)
+            }
         }
+
+        // Child Terminal
+        do {
+            let result = ioctl(childHandle.fileDescriptor, TIOCSWINSZ, &size)
+            if let errorCode = POSIXErrorCode(rawValue: result) {
+                throw POSIXError(errorCode)
+            }
+        }
+
     }
 
     public func getWindowSize() throws -> winsize {
